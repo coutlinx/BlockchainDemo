51cd06313c18254f1603145e6926a50adc974d9f
coutlinx <1402284023@qq.com>
2021-10-31 18:57:44 +0800

    V0.0.3

--- BlockChainDemo/public/web3js/index.js
+++ BlockChainDemo/public/web3js/index.js
@@ -1,32 +1,43 @@
-var accounts = [];
-var balance;
+
 if (typeof window.ethereum !== "undefined") {
   console.log("MetaMask is installed!");
-  getAccount((err, acc) => {
-    if (err) {
-      console.log(err);
-    }else{
-        console.log(acc)
-        accounts =acc;
-    }
-  });
-      getBalance((err, res) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(res);
+  var p1 = new Promise(getAccount);
+  p1.then((acc)=>{
+    accounts =acc;
+    accNum = acc.length
+    $("#accountAddress").html(acc[0]);
+    for (let i = 0; i < accNum; i++) {
+          $option = `<option>${acc[i]}</option>`;
+          $("#bidAccount").append($option);
+          console.log(acc[i]);
         }
-      });
+    getBalance((err,res)=>{
+      if(err){
+        console.log(err);
+      }else{
+        $("#accountBalance").html(parseInt(res/Math.pow(10,18))+"ETH")
+        console.log(res)
+      }
+    })
+  }).catch(err=>console.log(err));
+  // getAccount((err, acc) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }else{
+  //       console.log(acc)
+  //       accounts =acc;
+  //   }
+  // });
 
 } else {
   alter("plase install the MetaMask");
 }
 
-function getAccount(callback) {
+function getAccount(resolve,reject) {
   ethereum
     .request({ method: "eth_requestAccounts" })
-    .then((res) => callback(null, res))
-    .catch((err) => callback(err, null));
+    .then((res) => resolve(res))
+    .catch((err) => reject(err));
 
   //   accNum = accounts.length;
   //   const account = accounts[0];
@@ -44,9 +55,9 @@ function getBalance(callback) {
       params: [accounts[0], "latest"],
     })
     .then((res) => {
-      callback(null, res);
+      callback(null,res)
     })
     .catch((error) => {
-      callback(error, null);
+      callback(error,null);
     });
 }
