9aa7b6eea21d3d2336a4160f7468dd6eb394ce40
coutlinx <1402284023@qq.com>
2021-11-09 17:27:30 +0800

    V0.0.3

--- BlockChainDemo/routes/withdraw.js
+++ BlockChainDemo/routes/withdraw.js
@@ -3,4 +3,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('withdraw')
   });
+  router.post('/',(req,res)=>{
+    console.log(req.body);
+  })
   module.exports = router;
